f88260a21c25a360ac24607a67a631a3b1ff1e17
bruno-nakahara <Bruno_Nakahara95@yahoo.com>
2021-11-17 15:27:31 -0300

    add insert user function

--- pos-java-jdbc/src/main/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
+++ pos-java-jdbc/src/main/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
@@ -1,12 +1,22 @@
 package pos_java_jdbc.pos_java_jdbc;
 
 import conexaojdbc.SingleConnection;
+import dao.UserPosDAO;
+import model.Userposjava;
+
 import org.junit.Test;
 
 public class TesteBancoJdbc {
 
 	@Test
 	public void initBanco() {
-		SingleConnection.getConnection();
+		UserPosDAO userPosDAO = new UserPosDAO();
+		Userposjava userposjava = new Userposjava();
+		
+		userposjava.setId(5L);
+		userposjava.setNome("Diego Costa");
+		userposjava.setEmail("rock@jdc.com");
+		
+		userPosDAO.salvar(userposjava);
 	}
 }
